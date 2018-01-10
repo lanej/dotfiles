1c0dd337e91ab7a87ae422e62c824b75c9b97bad
Joshua Lane <me@joshualane.com>
2018-01-10 09:20:19 -0800

    [fish] fix release scripts

--- fish/functions/current-release.fish
+++ fish/functions/current-release.fish
@@ -1,4 +1,8 @@
 #!/usr/bin/env fish
 function current-release
-	git tag -l 'v*' | sed -E 's/^v//g' | sort -g | tail -n 1
+	set release (git tag -l 'v*' | sed -E 's/^v//g' | sort -g | tail -n 1)
+  if test -z $release
+    set release 0
+  end
+  echo $release
 end
--- fish/functions/git-release.fish
+++ fish/functions/git-release.fish
@@ -2,10 +2,10 @@ function git-release
 	git fetch origin -q
 	set current_release (current-release)
 	echo "Current release: v$current_release"
-	local next_release=v( math $current_release + 1 )
+	set next_release v( math $current_release + 1 )
 	echo "Releasing: $next_release"
-	git tag $next_release && git push origin $next_release
-  if ls Capfile
+	git tag $next_release; and git push origin $next_release
+  if ls Capfile > /dev/null
 	  printf "deploy with:\n\tREVISION=$next_release bundle exec cap production deploy"
   end
 end
