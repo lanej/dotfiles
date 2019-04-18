7cd6c23c47d379caa7b1c837add542f7742d613c
Joshua Lane <joshlane@easypost.com>
2019-04-18 11:18:21 -0700

    fix(z): use reverse with limited height
    Keeps everything on the line.

--- sh/z.sh
+++ sh/z.sh
@@ -1,3 +1,3 @@
 zz() {
-	cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf -q "$_last_z_args")" || exit
+	cd "$(_z -l 2>&1 | sed 's/^[0-9,.]* *//' | fzf --reverse --height=30% -q "$_last_z_args")" || exit
 }
