5d41d56ae3018222779b1fc65ced5a2804384b5c
Joshua Lane <me@joshualane.com>
2021-10-10 12:24:19 -0700

    refactor(nvim): easier simplify rust_analyzer, completion_servers

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -29,23 +29,17 @@ cmp.setup({
   },
 })
 
-require("lspconfig").rust_analyzer.setup {
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                               .make_client_capabilities()),
-}
+local completion_servers = { "rust_analyzer", "solargraph", "gopls" }
 
-require("lspconfig").solargraph.setup {
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                               .make_client_capabilities()),
-}
+for _, server in ipairs(completion_servers) do
+  require("lspconfig")[server].setup {
+    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
+                                                                 .make_client_capabilities()),
+  }
+end
 
 require("lspconfig").sumneko_lua.setup {
   cmd = { "lua-language-server" },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                               .make_client_capabilities()),
-}
-
-require("lspconfig").gopls.setup {
   capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
                                                                .make_client_capabilities()),
 }
