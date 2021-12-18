4acd376a1914b513e93e635db44a35adeced36ba
Joshua Lane <joshlane@easypost.com>
2021-12-17 21:38:58 -0800

    feat(nvim): let try signature completion for a while

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -1,45 +1,38 @@
 local cmp = require "cmp"
 
 cmp.setup({
-  snippet = {
-    expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
-  },
-  mapping = {
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      select = false,
-    }),
-  },
-  sources = {
-    {
-      name = "nvim_lsp",
+    snippet = {expand = function(args) vim.fn["vsnip#anonymous"](args.body) end},
+    mapping = {
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({select = false})
     },
-    {
-      name = "buffer",
-    },
-    {
-      name = "path",
-    },
-    {
-      name = "vsnip",
-    },
-  },
+    sources = {
+        {name = "nvim_lsp"}, {name = "buffer"}, {name = "path"},
+        {name = "vsnip"}
+    }
 })
 
-local completion_servers = { "rust_analyzer", "solargraph", "gopls" }
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+    properties = {"documentation", "detail", "additionalTextEdits"}
+}
 
+require("lsp_signature").setup({
+    bind = true,
+    border = "single",
+})
+local completion_servers = {"rust_analyzer", "solargraph", "gopls"}
 for _, server in ipairs(completion_servers) do
-  require("lspconfig")[server].setup {
-    capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                                 .make_client_capabilities()),
-  }
+    require("lspconfig")[server].setup {
+        capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+    }
 end
 
 require("lspconfig").sumneko_lua.setup {
-  cmd = { "lua-language-server" },
-  capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol
-                                                               .make_client_capabilities()),
+    cmd = {"lua-language-server"},
+    capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 }
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,6 +20,7 @@ return require("packer").startup({
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-vsnip",
+	"ray-x/lsp_signature.nvim",
       },
     }
     use {
