f9a6024383023a8bbeb70be664a15169802e7861
Joshua Lane <me@joshualane.com>
2022-01-02 21:43:35 -0800

    feat(nvim): nvim-tree diagnostics, keep netrw

--- nvim/lua/nvim-tree-config.lua
+++ nvim/lua/nvim-tree-config.lua
@@ -1,3 +1,22 @@
-vim.api.nvim_set_keymap('n', '<leader>ntt', '<cmd>:NvimTreeToggle<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ntc', '<cmd>:NvimTreeClose<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ntf', '<cmd>:NvimTreeFindFile<CR>', { noremap = true, silent = true })
+require("nvim-tree").setup({
+  disable_netrw = false,
+  hijack_netrw = true,
+  auto_close = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = true,
+  },
+})
+
+vim.api.nvim_set_keymap("n", "<leader>ntt", "<cmd>:NvimTreeToggle<CR>", {
+  noremap = true,
+  silent = true,
+})
+vim.api.nvim_set_keymap("n", "<leader>ntc", "<cmd>:NvimTreeClose<CR>", {
+  noremap = true,
+  silent = true,
+})
+vim.api.nvim_set_keymap("n", "<leader>ntf", "<cmd>:NvimTreeFindFile<CR>", {
+  noremap = true,
+  silent = true,
+})
