197f423be91934645a3dab84cc8d7efb4305a1d0
Joshua Lane <me@joshualane.com>
2022-08-28 15:14:57 -0700

    fix(nvim): don't need c-space for completion, select on confirm

--- nvim/lua/completion.lua
+++ nvim/lua/completion.lua
@@ -7,10 +7,9 @@ cmp.setup({
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({
-      select = false,
+      select = true,
     }),
   },
   sources = {
