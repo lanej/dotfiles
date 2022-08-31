be0067f5c4e2c810cd9472fe7e97b7ab5afcbe7d
Joshua Lane <me@joshualane.com>
2022-08-30 20:09:32 -0700

    fix(nvim): remove double keymap specification

--- nvim/lua/gitsignsconfig.lua
+++ nvim/lua/gitsignsconfig.lua
@@ -6,11 +6,11 @@ require('gitsigns').setup({
   },
 })
 
-vim.api.nvim_set_keymap('n', '<leader>gw', '<cmd><leader>gw :Gitsigns stage_buffer<CR>', {
+vim.api.nvim_set_keymap('n', '<leader>gw', ':Gitsigns stage_buffer<CR>', {
   silent = true,
   noremap = true,
 })
-vim.api.nvim_set_keymap('n', '<leader>gl', '<cmd><leader>gl :Gitsigns blame_line<CR>', {
+vim.api.nvim_set_keymap('n', '<leader>gl', ':Gitsigns blame_line<CR>', {
   silent = true,
   noremap = true,
 })
