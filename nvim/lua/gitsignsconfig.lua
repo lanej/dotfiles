a9a4b2a83b1caca76b1b4bf4c5e7c59c85e6c8a6
Joshua Lane <me@joshualane.com>
2022-10-11 09:08:34 -0700

    feat: gitsigns, smartyank, silicon

--- nvim/lua/gitsignsconfig.lua
+++ nvim/lua/gitsignsconfig.lua
@@ -14,4 +14,31 @@ vim.api.nvim_set_keymap('n', '<leader>gl', ':Gitsigns blame_line<CR>', {
   silent = true,
   noremap = true,
 })
-
+vim.api.nvim_set_keymap('n', ']c', ':Gitsigns next_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', '[c', ':Gitsigns prev_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', '<leader>hp', ':Gitsigns preview_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', '<leader>hs', ':Gitsigns stage_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', '<leader>hr', ':Gitsigns reset_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', '<leader>hu', ':Gitsigns undo_stage_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
+vim.api.nvim_set_keymap('n', 'vh', ':Gitsigns select_hunk<CR>', {
+  silent = true,
+  noremap = true,
+})
