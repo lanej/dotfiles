e47d0aac920df9f4a2d8743984efbe837442cd2e
Joshua Lane <me@joshualane.com>
2023-01-01 15:50:53 -0800

    style: remove whitespace; formatting

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -42,7 +42,6 @@ cmp.setup({
     ['<C-j>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-
     ['<CR>'] = function(fallback)
       if cmp.visible() then
         cmp.confirm({ select = true }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
@@ -51,21 +50,25 @@ cmp.setup({
       end
     end
   }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' }, -- For vsnip users.
-  }, {
-    { name = 'buffer' },
-  })
+  sources = cmp.config.sources(
+    {
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' },
+    },
+    {
+      { name = 'buffer' },
+    })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+  sources = cmp.config.sources(
+    {
+      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    },
+    {
+      { name = 'buffer' },
+    })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -79,11 +82,13 @@ cmp.setup.cmdline({ '/', '?' }, {
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+  sources = cmp.config.sources(
+    {
+      { name = 'path' }
+    },
+    {
+      { name = 'cmdline' }
+    })
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
