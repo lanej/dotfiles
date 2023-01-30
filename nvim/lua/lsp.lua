3ffbcf3c26572752c74f6bd2c4506dacfcb5a832
Joshua Lane <joshlane@easypost.com>
2023-01-30 02:36:25 +0000

    fix: fallback on cmp Tab/S-Tab

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -28,14 +28,18 @@ cmp.setup({
     end
   },
   mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function()
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if vim.fn["vsnip#available"](1) == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      else
+        fallback()
       end
     end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function()
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
+      else
+        fallback()
       end
     end, { "i", "s" }),
     ['<C-k>'] = cmp.mapping.scroll_docs(-4),
