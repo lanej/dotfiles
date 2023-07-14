8bed4a1458f8b43157aeecaba0439c9f296885a1
Joshua Lane <joshlane@easypost.com>
2023-07-14 14:57:01 -0700

    refactor(nvim): better window choices

--- nvim/lua/nvim-window-config.lua
+++ nvim/lua/nvim-window-config.lua
@@ -1,20 +1,15 @@
-  require('nvim-window').setup({
-    -- The characters available for hinting windows.
-    chars = {
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-      'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-    },
-
-    -- A group to use for overwriting the Normal highlight group in the floating
-    -- window. This can be used to change the background color.
-    normal_hl = 'Type',
-
-    -- The highlight group to apply to the line that contains the hint characters.
-    -- This is used to make them stand out more.
-    hint_hl = 'Bold',
-
-    -- The border style to use for the floating window.
-    border = 'double'
+require('nvim-window').setup({
+  -- The characters available for hinting windows.
+  chars = { 'j', 'k', 'l', 'h', 'a', 's', 'd', 'f' },
+  -- A group to use for overwriting the Normal highlight group in the floating
+  -- window. This can be used to change the background color.
+  normal_hl = 'Type',
+  -- The highlight group to apply to the line that contains the hint characters.
+  -- This is used to make them stand out more.
+  hint_hl = 'Bold',
+  -- The border style to use for the floating window.
+  border = 'double'
 })
 
-vim.api.nvim_set_keymap('n', '<c-w><c-w>','<cmd>lua require("nvim-window").pick()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<c-w><c-w>', '<cmd>lua require("nvim-window").pick()<CR>',
+  { noremap = true, silent = true })
