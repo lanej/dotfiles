f835213b82eb3972b78fa710442114c5af2e4add
Joshua Lane <joshlane@easypost.com>
2024-04-21 01:22:59 +0000

    feat(nvim/fzf): phab wiring and better changed file formatting

--- nvim/lua/fzfconfig.lua
+++ nvim/lua/fzfconfig.lua
@@ -1,7 +1,5 @@
 local fzf_lua = require 'fzf-lua'
 local actions = require 'fzf-lua.actions'
-local utils = require "fzf-lua.utils"
-local path = require "fzf-lua.path"
 
 -- TODO: git diff --cached
 
@@ -18,10 +16,19 @@ require('fzf-lua').setup {
       ['ctrl-b'] = 'toggle-all',
     },
   },
-  winopts = {
-    preview = {
-      flip_columns = 180, -- #cols to switch to horizontal on flex
-    },
+  -- winopts = {
+  --   height  = 0.85,             -- window height
+  --   width   = 0.80,             -- window width
+  --   row     = 0.35,             -- window row position (0=top, 1=bottom)
+  --   col     = 0.50,             -- window col position (0=left, 1=right)
+  --   preview = {
+  --     vertical   = 'down:62%',  -- up|down:size
+  --     horizontal = 'right:62%', -- right|left:size
+  --   },
+  -- },
+  buffers = {
+    sort_lastused = true,
+    ignore_current_buffer = true,
   },
   git = {
     branches = {
@@ -33,7 +40,7 @@ require('fzf-lua').setup {
       },
     },
     commits = {
-      preview = 'echo {} | awk \'{ print $1 }\' | xargs git show | delta',
+      preview = "echo {} | awk '{ print $1 }' | xargs git show | delta",
       actions = {
         ['default'] = actions.git_buf_edit,
         ['ctrl-s'] = actions.git_buf_split,
@@ -171,8 +178,59 @@ vim.api.nvim_set_keymap('n', '<leader>j', '<cmd>lua require("fzf-lua").jumps()<C
   silent = true,
 })
 
+vim.keymap.set({ 'n' }, '<leader>dc', function()
+  require "fzf-lua".fzf_exec(function(callback)
+    local comments = require("phab").get_comments()
+    print(vim.inspect(comments))
+
+    for _, comment in ipairs(comments) do
+      callback(comment.author .. ": " .. comment.comment)
+    end
+
+    callback()
+  end, {
+    prompt = "Diff Comments> ",
+    previewer = false,
+    -- preview = require 'fzf-lua'.shell.raw_preview_action_cmd(function(items) return vim.inspect(items) end),
+    fn_transform = function(x)
+      return require 'fzf-lua'.make_entry.file(x, { file_icons = true, color_icons = true })
+    end,
+  })
+end, { noremap = true, silent = true })
+
+-- Mtag = function(x, opts)
+--   local name, file, text = x:match("([^\t]+)\t([^\t]+)\t(.*)")
+--   if not file or not name or not text then return x end
+--   text = text:match([[(.*);"]]) or text -- remove ctag comments
+--   -- unescape ctags special chars
+--   -- '\/' -> '/'
+--   -- '\\' -> '\'
+--   for _, s in ipairs({ "/", "\\" }) do
+--     text = text:gsub([[\]] .. s, s)
+--   end
+--   local line, tag = text:match("(%d-);?(/.*/)")
+--   line = line and #line > 0 and tonumber(line)
+--   return ("%s%s: %s %s"):format(
+--     M.file(file, opts),
+--     not line and "" or ":" .. utils.ansi_codes.green(tostring(line)),
+--     utils.ansi_codes.magenta(name),
+--     utils.ansi_codes.green(tag)
+--   ), line
+-- end
+
+
 -- https://github.com/ibhagwan/fzf-lua/wiki/Advanced#keybind-handlers
-vim.api.nvim_set_keymap('n', '<leader>cf', '<cmd>lua require("fzf-lua").files({ cmd = "git diff $(git merge-base --fork-point origin/master) --name-only", preview = "echo {} | xargs -n 1 -I {} git diff $(git merge-base --fork-point origin/master) --shortstat --no-prefix -U25 -- {} | delta", actions = require("fzf-lua").defaults.actions.files })<CR>', {
-  noremap = true,
-  silent = true,
-})
+vim.keymap.set({ 'n' }, '<leader>cf', function()
+    require 'fzf-lua'.fzf_exec(
+      "git diff $(git merge-base --fork-point origin/master) --name-only --diff-filter=AM",
+      {
+        actions = require("fzf-lua").defaults.actions.files,
+        preview =
+        "echo {} | xargs -n 1 -I {} git diff $(git merge-base --fork-point origin/master) --shortstat --no-prefix -U25 -- {} | delta",
+      }
+    )
+  end,
+  {
+    noremap = true,
+    silent = true,
+  })
