5afc82e715b6d18befff967d657a78182cbb7674
Joshua Lane <joshlane@easypost.com>
2024-04-24 05:06:33 +0000

    fix(nvim): use init to prevent tmux navigator overriding keymaps

--- nvim/lua/tmux-config.lua
+++
@@ -1,7 +0,0 @@
-vim.g.tmux_navigator_no_mappings =1
-vim.g.tmux_navigator_save_on_switch = 1
-
-vim.api.nvim_set_keymap('n', '<C-h>',':TmuxNavigateLeft<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-j>',':TmuxNavigateDown<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-k>',':TmuxNavigateUp<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-l>',':TmuxNavigateRight<cr>', { noremap = true, silent = true })
