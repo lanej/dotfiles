5d4d95cd954458f896e2a83d8a83e484c5926ad9
Joshua Lane <joshlane@easypost.com>
2025-01-11 20:26:50 -0800

    feat(nvim): add inlay hint extmark plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1540,6 +1540,28 @@ require("lazy").setup({
 		},
 	},
 	{
+		"chrisgrieser/nvim-lsp-endhints",
+		event = "LspAttach",
+		opts = {
+			icons = {
+				type = " ",
+				parameter = " ",
+				offspec = " ", -- hint kind not defined in official LSP spec
+				unknown = " ", -- hint kind is nil
+			},
+			label = {
+				truncateAtChars = 20,
+				padding = 1,
+				marginLeft = 0,
+				sameKindSeparator = ", ",
+			},
+			extmark = {
+				priority = 50,
+			},
+			autoEnableHints = true,
+		},
+	},
+	{
 		"nvim-treesitter/nvim-treesitter-context",
 		opts = {
 			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
