d6bc2f4014e610fbcc9f9608fb1ad56cb95de1e0
Josh Lane <lanej@users.noreply.github.com>
2025-02-23 19:14:54 -0800

    fix(fzfconfig): disable compatibility warning
    The compatibility warning in fzf-lua's setup was set to depend on
    the presence of skim. This has been changed to always be false
    to prevent unnecessary warnings during usage.

--- nvim/lua/fzfconfig.lua
+++ nvim/lua/fzfconfig.lua
@@ -8,7 +8,7 @@ require("fzf-lua").setup({
 	files = {
 		cmd = "fd --type f --hidden --follow --exclude .git",
 	},
-	defaults = { compat_warn = not skim_present },
+	defaults = { compat_warn = false },
 	keymap = {
 		builtin = {
 			["<C-f>"] = "toggle-fullscreen",
